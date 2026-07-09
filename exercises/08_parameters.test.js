de22f5e4e49be6ccb8a4ad8a1949c03de901a80e
Cris Mouta <82060703+crismouta@users.noreply.github.com>
2026-07-09 13:10:08 +0200

    fix: improve validation of 08_parameters-2

--- exercises/08_parameters.test.js
+++ exercises/08_parameters.test.js
@@ -9,7 +9,9 @@ test('08_parameters-1: can be triggered when the incoming argument is undefined'
 })
 
 test('08_parameters-2: aren\'t included in arguments', () => {
-  const getName = (name = 'Mercury') => arguments.length
+  const getName = function (name = 'Mercury') {
+    return arguments.length
+  }
 
   // Comprova que els paràmetres per defecte no formen part de l'objecte `arguments`
   expect(getName('Aaron')).toBe(/*INTRODUEIX LA TEVA RESPOSTA AQUÍ*/)
