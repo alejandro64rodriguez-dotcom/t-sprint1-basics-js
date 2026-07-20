ac170bc7fb549c1d9dfdd62e2d8223ac42326218
mat <alejandro64rodriguez@gmail.com>
2026-07-20 11:20:10 +0200

    test: reflect

--- exercises/21_reflect.test.js
+++ exercises/21_reflect.test.js
@@ -6,7 +6,7 @@ test('21_reflect-1: Reflect.apply can be used to call a function', () => {
     },
   }
 
-  const result = null // utilitza Reflect.apply per invocar person.sayHi
+  const result = Reflect.apply(person.sayHi, person, ['Hey there', 'Jaimee'])
   expect(result).toBe('Hey there Jaimee! My name is Fred')
 })
 
@@ -16,14 +16,14 @@ test('21_reflect-2: Reflect.deleteProperty can be used instead of the `delete` k
     configurable: false,
     value: 'YOU CANNOT GET RID OF ME!',
   })
-  // utilitza Reflect.deleteProperty per eliminar la propietat age de l'objecte person
-  const ageDeleted = delete person.age
-  const protectedDeleted = delete person.protected
+  const ageDeleted = Reflect.deleteProperty(person, 'age')
+  const protectedDeleted = Reflect.deleteProperty(person, 'protected')
   expect(person.age).not.toBeDefined()
   expect(ageDeleted).toBe(true)
   expect(person.protected).toBe('YOU CANNOT GET RID OF ME!')
   expect(protectedDeleted).toBe(false)
 })
+
 
 test(`21_reflect-3: Reflect.ownKeys returns the object's own (not inherited) keys (including symbols)`, () => {
   const exists = Symbol('existance')
@@ -39,19 +39,9 @@ test(`21_reflect-3: Reflect.ownKeys returns the object's own (not inherited) key
     configurable: false,
     enumerable: false,
   })
-  // pista, les claus estaran en l'ordre en què s'afegeixen a l'objecte
-  // això serà el cas per a la majoria d'entorns, tot i que generalment no és
-  // una bona idea confiar en aquest fet, ja que no està especificat a l'especificació.
-  expect(Object.keys(kyle)).toEqual([
-    /* INTRODUEIX LA TEVA RESPOSTA AQUÍ */
-  ])
-  expect(Object.getOwnPropertyNames(kyle)).toEqual([
-    /* INTRODUEIX LA TEVA RESPOSTA AQUÍ */
-  ])
-  expect(Object.getOwnPropertySymbols(kyle)).toEqual([
-    /* INTRODUEIX LA TEVA RESPOSTA AQUÍ */
-  ])
-  expect(Reflect.ownKeys(kyle)).toEqual([
-    /* INTRODUEIX LA TEVA RESPOSTA AQUÍ */
-  ])
+
+  expect(Object.keys(kyle)).toEqual(['awesome'])
+  expect(Object.getOwnPropertyNames(kyle)).toEqual(['awesome', 'favoriteLanguage'])
+  expect(Object.getOwnPropertySymbols(kyle)).toEqual([favoriteFeature])
+  expect(Reflect.ownKeys(kyle)).toEqual(['awesome', 'favoriteLanguage', favoriteFeature])
 })
